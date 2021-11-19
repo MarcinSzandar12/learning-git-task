364441ac8a5f1fb3ce911a4ce56536db744927c8
Marcin Szandar <martin0998@interia.pl>
2021-11-19 13:44:48 +0100

    init project shopping_list.py

---
+++ shopping_list.py
@@ -0,0 +1,10 @@
+print("Lista zakupów")
+
+shopping_list = {"piekarnia":["chleb", "bułki", "pączek"], "warzywniak":["marchew", "seler", "rukola"]}
+
+#shopping_list = [product.capitalize() for product in shopping_list.values()]
+
+for x, y in shopping_list.items():
+    print("Idę do", x.capitalize(),"kupuję tu następujące rzeczy:", y)
+
+print("W sumie kupuję", len(shopping_list["piekarnia"]) + len(shopping_list["warzywniak"]), "produktów.")
