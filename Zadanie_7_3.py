0bcb8202e0b16e5cafdd44b819bf99b552615f62
Marcin Szandar <martin0998@interia.pl>
2022-01-17 13:02:56 +0100

    add Zadanie_7_3.py

--- Zadanie_7_3.py
+++ Zadanie_7_3.py
@@ -1,11 +1,12 @@
+from faker import Faker
+fake = Faker()
+
 class BaseContact:
     def __init__(self, first_name, last_name, phone_number, email):
        self.first_name = first_name
        self.last_name = last_name
        self.phone_number = phone_number
        self.email = email
-       
-       self.name_length = int(len(first_name) + len(last_name))
        
     def __str__(self):
         return f'{self.first_name} {self.last_name} {self.email}'
@@ -15,7 +16,7 @@ class BaseContact:
 
     @property
     def label_length(self):
-        return f'{self.name_length}'
+        return int(len(self.first_name) + len(self.last_name))
 
 class BusinessContact(BaseContact):
     def __init__(self, business_phone, company_name, job_position, *args, **kwargs):
@@ -26,10 +27,6 @@ class BusinessContact(BaseContact):
 
     def contact(self):
         return f'Wybieram numer {self.business_phone} i dzwonię do {self.first_name} {self.last_name}'
-
-    @property
-    def label_length(self):
-        return f'{self.name_length}'
 
 card_1 = BaseContact(first_name = "Karol", last_name = "Nowak", phone_number = "+48 409 054 993", email = "KarolNowak@rhyta.com")
 card_2 = BaseContact(first_name = "Sławomira", last_name = "Zając", phone_number = "+48 395 237 012", email = "SlawomiraZajac@dayrep.com")
@@ -43,21 +40,23 @@ card_8 = BusinessContact(first_name = "Bartosz", last_name = "Rutkowski", phone_
 card_9 = BusinessContact(first_name = "Oliwia", last_name = "Czarnecka", phone_number = "+48 142 576 978", business_phone = "+48 519 945 920", company_name = "Greenwich IGA", job_position = "Pipelayer", email = "OliwiaCzarnecka@armyspy.com")
 card_10 = BusinessContact(first_name = "Kunegunda", last_name = "Kwiatkowska", phone_number = "+48 135 246 357", business_phone = "+48 111 232 909", company_name = "Weingarten's", job_position = "Power transformer repairer", email = "KunegundaKwiatkowska@rhyta.com")
 
-cards = [card_1, card_2, card_3, card_4, card_5]
+#cards = [card_1, card_2, card_3, card_4, card_5]
 
 by_first_name = sorted(cards, key=lambda card: card.first_name)
 by_last_name = sorted(cards, key=lambda card: card.last_name)
 by_email = sorted(cards, key=lambda card: card.email)
 
-from faker import Faker
-def Create_card(card_type, quantity):
-    if card_type = "C":
+
+def create_card(card_type, quantity):
+    cards = []
+    if card_type == "C":
         for i in range(quantity):
-            fake_data = [fake.first_name(), fake.last_name(), fake.phone_number(), fake.free_email()]
-            return fake_data
-    elif card_type = "B":
+            card = BaseContact(first_name = fake.first_name(), last_name = fake.last_name(), phone_number = fake.phone_number(), email = fake.free_email())
+            cards.append(card) 
+    elif card_type == "B":
         for i in range(quantity):
-            fake_data = [fake.first_name(), fake.last_name(), fake.phone_number(), fake.company(), fake.job(), fake.free_email()]
-            return fake_data
+            card = BaseContact(first_name = fake.first_name(), last_name = fake.last_name(), phone_number = fake.phone_number(), fake_company = fake.company(), fake_job = fake.job(), email = fake.free_email())
+            cards.append(card) 
     else:
-        return 'Wprowadziłeś błędne dane.'
+        print('Wprowadziłeś błędne dane.')
+    return cards
